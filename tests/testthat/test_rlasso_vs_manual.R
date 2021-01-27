606cca264f942057285451de14653d5f352e9c9d
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-01-27 12:10:45 +0100

    also add a unit test for the double selection (only coeffs not se)

--- tests/testthat/test_rlasso_vs_manual.R
+++ tests/testthat/test_rlasso_vs_manual.R
@@ -6,7 +6,7 @@ set.seed(1)
 sim_data <- DGP_rlasso_hdm_paper_sec4(n=5000, p=20)
 
 test_cases <- expand.grid(post = c(TRUE, FALSE),
-                          method = c('partialling out'))
+                          method = c('partialling out', 'double selection'))
 test_cases['test_name'] <- apply(test_cases, 1, paste, collapse="_")
 
 manual_partial_out <- function(data, x_cols, post){
@@ -18,15 +18,34 @@ manual_partial_out <- function(data, x_cols, post){
   return(partial.fit)
 }
 
+manual_double_selection <- function(data, x_cols, post){
+  fmla.y <- as.formula(paste('y ~ ',paste(x_cols, collapse='+')))
+  fmla.d <- as.formula(paste('d ~ ',paste(x_cols, collapse='+')))
+  rY <- rlasso(fmla.y, data=data, post=post)$coefficients
+  rD <- rlasso(fmla.d, data=data, post=post)$coefficients
+  
+  sel_x = xnames[abs(betaY[xnames])>0 | abs(betaD[xnames])>0]
+  fmla.double.sel <- as.formula(paste('y ~ ', paste(c("d", sel_x), collapse='+')))
+  partial.fit <- lm(fmla.double.sel, sim_data)
+  return(partial.fit)
+}
+
 patrick::with_parameters_test_that("Unit tests for rlassoEffect vs manual:", .cases = test_cases, {
   xnames <- colnames(sim_data)[-c(1,2)]
   
-  partial.fit <- manual_partial_out(sim_data, xnames, post=post)
+  if (method == 'partialling out') {
+    partial.fit <- manual_partial_out(sim_data, xnames, post=post)
+  } else if (method == 'double selection') {
+    partial.fit <- manual_double_selection(sim_data, xnames, post=post)
+  }
   
   Eff = rlassoEffect(as.matrix(sim_data[, xnames]), sim_data['y'], sim_data['d'],
                      method=method, post=post)
   
-  expect_equal(summary(partial.fit)$coef['rD', 1:2], summary(Eff)$coef[,1:2], tolerance = 1e-8)
-  
+  if (method == 'partialling out') {
+    expect_equal(summary(partial.fit)$coef['rD', 1:2], summary(Eff)$coef[,1:2], tolerance = 1e-8)
+  } else if (method == 'double selection') {
+    expect_equal(partial.fit$coefficients['d'], Eff$alpha, tolerance = 1e-8)
+  }
 }
 )
