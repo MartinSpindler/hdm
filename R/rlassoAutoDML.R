971d7f3965418f4c856c608325b87d16c8272dd0
Philipp Bach <bach.philipp@outlook.com>
2021-08-19 15:14:15 +0200

    rename rlassoDML to rlassoAutoDML and use this name in filenames

---
+++ R/rlassoAutoDML.R
@@ -0,0 +1,130 @@
+# library(hdm)
+# library(mvtnorm)
+
+
+# functional only uses lasso in hdm framework/ keep within hdm framework
+# object oriented is more flexible/can use more learners
+# cross check functional/object oriented
+# prepare data example on object oriented
+# check whether standard errors/scores match expectation
+
+
+# For functional Docs, examples, testing
+# code insert Roxygen skeleton
+# user passes by hand modern matrix / build wrapper later
+# supports different interfaces
+
+
+
+
+#' DML functional
+#'
+#' @param Y A vector of outputs
+#' @param D A vector of treatment values
+#' @param X A matrix of covariates
+#' @param dict A dictionary
+#' @return DML results
+#' @examples
+#' rlassoDML(Y, T, X, dict)
+#' @export
+#' @rdname rlassoDML
+rlassoAutoDML <- function(Y, D, X, dict, D_LB = 0, D_add = 0.2, bias = FALSE, L = 5) {
+  # Inputs
+  # Y: output variable
+  # T: treatment variable
+  # X: covariates
+  # bias: debiased vs. biased results
+  # L: number of folds
+  #
+  # Output
+  # list with average treatment effect and standard error
+
+  p <- length(dict(D[1], X[1, ]))
+
+  # p0=dim(X0) used in low-dim dictionary in the stage 1 tuning procedure
+  p0 <- ceiling(p / 4)
+  if (p > 60) {
+    p0 <- ceiling(p / 40)
+
+  }
+
+  max_iter <- 10 # max number iterations in Dantzig selector iteration over estimation and weights
+
+
+  n <- nrow(X)
+  folds <- split(sample(n, n, replace = FALSE), as.factor(1:L))
+
+  Psi_tilde <- numeric(0)
+
+  for (l in 1:L) {
+
+    # folds[[1]] = test_inds_global
+
+    Y.l <- Y[folds[[l]]]
+    Y.nl <- Y[-folds[[l]]]
+
+    T.l <- D[folds[[l]]]
+    T.nl <- D[-folds[[l]]]
+
+    X.l <- X[folds[[l]], ]
+    X.nl <- X[-folds[[l]], ]
+
+
+    n.l <- length(T.l)
+    n.nl <- length(T.nl)
+
+    # get stage 1 (on nl)
+
+    rho_hat <- RMD_stable(Y.nl, T.nl, X.nl, p0, D_LB, D_add, max_iter, dict)
+
+    alpha_hat <- function(d, z) {
+      return(dict(d, z) %*% rho_hat)
+    }
+
+
+    n <- nrow(X.nl)
+    p <- length(dict(T.nl[1], X.nl[1, ]))
+    # Apply the dictionary b to W
+    # print(n)
+    # print(p)
+    B <- matrix(0, n, p)
+    for (i in 1:n) {
+      B[i, ] <- dict(T.nl[i], X.nl[i, ])
+    }
+
+    gamma_coeff <- rlasso(B, Y.nl, intercept = F)$coefficients
+    gamma_hat <- function(d, z) {
+      return(dict(d, z) %*% gamma_coeff)
+    }
+
+    print(paste0("fold: ", l))
+
+    # get stage 2 (on l)
+    # psi_star
+    Psi_tilde.l <- rep(0, n.l)
+    for (i in 1:n.l) {
+      if (bias) { # plug-in
+        Psi_tilde.l[i] <- psi_tilde_bias(Y.l[i], T.l[i], X.l[i, ], m, alpha_hat, gamma_hat) # without subtracting theta_hat
+      } else { # DML
+        Psi_tilde.l[i] <- psi_tilde(Y.l[i], T.l[i], X.l[i, ], m, alpha_hat, gamma_hat) # without subtracting theta_hat
+      }
+    }
+
+    Psi_tilde <- c(Psi_tilde, Psi_tilde.l)
+
+
+  }
+
+  # point estimation
+  ate <- mean(Psi_tilde)
+
+  # influences
+  Psi <- Psi_tilde - ate
+
+  var <- mean(Psi^2)
+  se <- sqrt(var / n)
+
+  out <- c(table(D)[[2]], table(D)[[1]], ate, se)
+
+  return(out)
+}
---
+++ R/rlassoAutoDML_helpers.R
@@ -0,0 +1,131 @@
+two.norm <- function(x) {
+  return(sqrt(x %*% x))
+}
+
+m <- function(y, d, z, gamma) { # all data arguments to make interchangeable with m2
+  return(gamma(1, z) - gamma(0, z))
+}
+
+m2 <- function(y, d, z, gamma) {
+  return(y * gamma(d, z))
+}
+
+psi_tilde <- function(y, d, z, m, alpha, gamma) {
+
+  return(m(y, d, z, gamma) + alpha(d, z) * (y - gamma(d, z)))
+}
+
+psi_tilde_bias <- function(y, d, z, m, alpha, gamma) {
+  return(m(y, d, z, gamma))
+}
+
+get_MNG <- function(Y, D, X, b) {
+
+  p <- length(b(D[1], X[1, ]))
+  n.nl <- length(D)
+
+  B <- matrix(0, n.nl, p)
+  M <- matrix(0, p, n.nl)
+  N <- matrix(0, p, n.nl)
+
+  for (i in 1:n.nl) {
+    B[i, ] <- b(D[i], X[i, ])
+    M[, i] <- m(Y[i], D[i], X[i, ], b)
+    N[, i] <- m2(Y[i], D[i], X[i, ], b) # this is a more general formulation for N
+  }
+
+  M_hat <- rowMeans(M)
+  N_hat <- rowMeans(N)
+  G_hat <- t(B) %*% B / n.nl
+
+  return(list(M_hat, N_hat, G_hat, B))
+}
+
+
+get_D <- function(Y, D, X, m, rho_hat, b) {
+  n <- nrow(X)
+  p <- length(b(D[1], X[1, ]))
+
+  df <- matrix(0, p, n)
+  for (i in 1:n) {
+    df[, i] <- b(D[i], X[i, ]) * as.vector(rho_hat %*% b(D[i], X[i, ])) - m(Y[i], D[i], X[i, ], b)
+  }
+  df <- df^2
+  D2 <- rowMeans(df)
+
+  D <- sqrt(D2)
+  return(D) # pass around D as vector
+}
+
+RMD_stable <- function(Y, D, X, p0, D_LB, D_add, max_iter, b, is_alpha, is_lasso) {
+
+  k <- 1
+  l <- 0.1
+
+  p <- length(b(D[1], X[1, ]))
+  n <- length(D)
+
+  # low-dimensional moments
+  X0 <- X[, 1:p0]
+  MNG0 <- get_MNG(Y, D, X0, b)
+  M_hat0 <- MNG0[[1]]
+  N_hat0 <- MNG0[[2]]
+  G_hat0 <- MNG0[[3]]
+
+  # initial estimate
+  rho_hat0 <- solve(G_hat0, M_hat0)
+  rho_hat <- c(rho_hat0, rep(0, p - ncol(G_hat0)))
+  beta_hat0 <- solve(G_hat0, N_hat0)
+  beta_hat <- c(beta_hat0, rep(0, p - ncol(G_hat0)))
+
+  # moments
+  MNG <- get_MNG(Y, D, X, b)
+  M_hat <- MNG[[1]]
+  N_hat <- MNG[[2]]
+  G_hat <- MNG[[3]]
+
+  c <- 0.5
+  alpha <- 0.1
+  tol <- 1e-6
+
+  # penalty
+  lambda <- c * qnorm(1 - alpha / (2 * p)) / sqrt(n) # snippet
+
+  ###########
+  # alpha_hat
+  ###########
+  diff_rho <- 1
+  while (diff_rho > tol & k <= max_iter) {
+
+    # previous values
+    rho_hat_old <- rho_hat + 0
+
+    # normalization
+    D_hat_rho <- get_D(Y, D, X, m, rho_hat_old, b)
+    D_hat_rho <- pmax(D_LB, D_hat_rho)
+    D_hat_rho <- D_hat_rho + D_add
+
+    # RMD estimate
+    # rho_hat=RMD_lasso(M_hat, G_hat, D_hat_rho, lambda)$coefficients
+
+    L <- c(l, rep(1, p - 1)) # dictionary is ordered (constant,...)
+    lambda_vec <- lambda * L * D_hat_rho # v3: insert D here
+    rho_hat <- LassoShooting.fit(G_hat, M_hat, lambda_vec, XX = -G_hat / 2, Xy = -M_hat / 2, beta.start = rep(0, p))$coefficients
+    # difference
+    diff_rho <- two.norm(rho_hat - rho_hat_old)
+    k <- k + 1
+
+  }
+
+  return(rho_hat)
+}
+
+
+
+printer <- function(spec1) {
+  print(paste(" treated: ", spec1[1], " untreated: ", spec1[2], "   ATE:    ", round(spec1[3], 2), "   SE:   ", round(spec1[4], 2), sep = ""))
+}
+
+for_tex <- function(spec1) {
+  print(paste(" & ", spec1[1], " & ", spec1[2], "   &    ", round(spec1[3], 2), "   &   ", round(spec1[4], 2), sep = ""))
+}
