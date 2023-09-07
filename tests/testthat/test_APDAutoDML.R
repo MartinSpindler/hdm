d123e757a938baaf815657797069e7fde2f91d30
PhilippBach <philipp.bach@uni-hamburg.de>
2023-09-07 14:50:14 +0200

    unit tests for APD

--- tests/testthat/test_APDAutoDML.R
+++ tests/testthat/test_APDAutoDML.R
@@ -2,6 +2,7 @@ context("AutoDML")
 library(hdm)
 library(testthat)
 library(data.table)
+library(MASS)
 
 DGP_contD <- function(n, p){
   X <- matrix(rnorm(n * p), ncol = p)
@@ -13,69 +14,72 @@ DGP_contD <- function(n, p){
   return(list("data" = DT, "true" = mean(1 + 2*DT$D + 3*DT$D^2 + DT$V1)))
 }
 
+DGP_panel <- function(N, N_T, p){
+    a =rnorm(N, 1, 1)
+    fixed =rep(1:N, each = N_T)
+    time =rep(1:N_T,  N)
+    DT <- data.table(fixed, time, a = rep(a, each = N_T))
+    size=p
+    k = diag(size) 
+    sigma = k
+    theta = 1/(1:p)^2
+    X = (Reduce(rbind, lapply(1:nrow(DT), function(i){mvrnorm(n =1, rep(DT$a[i], size), sigma)})))
+    DT <- cbind(DT, setNames(data.table(X), paste0("X_", 1:p)))
+    DT$D <- .1*(X%*%theta) + rbeta(N, 1, 7)
+    DT$y <- DT$a + DT$D  + DT$D^2 + DT$D^3 + DT$D*DT$X_1 +  .1*(X%*%theta) + rnorm(N, 0,1)
+    
+    return(list("data" = DT, "true" = mean(1 + 2*DT$D + 3*DT$D^2 + DT$X_1)))
+}
 
-set.seed(2)
-data_gen <- DGP_contD(100, 10)
-df <- data_gen$data
+test_cases = expand.grid(
+  panel_data = c(FALSE, TRUE),
+  L = c(2, 5),
+  gamma_learner = c("rlasso", "cv.glmnet"),
+  est_type = "APD",
+  poly_degree = c(2, 4),
+  interactions = c(FALSE, TRUE),
+  # weights = c(NULL, weights_vec),
+  debiased = c(TRUE, FALSE),
+  D_LB = c(0, 0.01),
+  D_add = c(0.2, 0.25),
+  stringsAsFactors = FALSE)
 
-# add intercept
-df$intercept <- rep(1, nrow(df))
-theta_true <- data_gen$true
+test_cases[".test_name"] = apply(test_cases, 1, paste, collapse = "_")
 
-poly_degree = 3
-
-Xnames = colnames(df)[grep("V", colnames(df))][-1]
-Xmanual = colnames(df)[grep("V", colnames(df))][1]
-
-backend_contD = DataAPDAutoDML(x = Xnames, d = "D", y = "y", x_manual = Xmanual, data = df, poly = poly_degree, intercept = "intercept")
-
-auto_apd = rlassoAutoDML(backend_contD, est_type = "APD")
-
-
-
-
-# cases
-poly = c(2, 3)
-interactions = c(TRUE, FALSE)
-
-#####
-
-
-
-test_that("rlassoEffect - Input check x, y and d", {
-  expect_is(rlassoEffect(X, y, d = X[, 1]), "rlassoEffects")
-  expect_is(rlassoEffect(X, as.vector(y), d = X[, 1]), "rlassoEffects")
-  expect_is(rlassoEffect(X[, 1, drop = FALSE], y, d = X[, 1]), "rlassoEffects")
-  expect_is(rlassoEffect(X[, 1, drop = FALSE], as.vector(y), d = X[, 1]), "rlassoEffects")
-})
-
-test_that("rlassoEffect - Input check I3", {
-  expect_is(rlassoEffect(X, y, d = X[, 1], I3 = c(rep(TRUE, 2), rep(FALSE, 2), TRUE)), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], I3 = c(rep(TRUE, 55), rep(FALSE, 44), TRUE)), "rlassoEffects")
-})
-
-test_that("rlassoEffect - Input check post, intercept and normalize", {
-  expect_is(rlassoEffect(X, y, d = X[, 1], post = FALSE), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], intercept = FALSE), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], normalize = FALSE), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], normalize = FALSE, intercept = FALSE), "rlassoEffects")
-})
-
-test_that("rlassoEffect - Input check penalty", {
-  expect_is(rlassoEffect(X, y, d = X[, 1], penalty = list(homoscedastic = FALSE, X.dependent.lambda = FALSE)), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], penalty = list(homoscedastic = TRUE, X.dependent.lambda = FALSE)), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], penalty = list(homoscedastic = FALSE, X.dependent.lambda = TRUE, numSim = 4000)), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], penalty = list(homoscedastic = TRUE, X.dependent.lambda = TRUE, numSim = 4000)), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], penalty = list(homoscedastic = "none", X.dependent.lambda = FALSE, lambda.start = 100)), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], penalty = list(homoscedastic = "none", X.dependent.lambda = TRUE, lambda.start = 100)), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], intercept = FALSE, penalty = list(homoscedastic = "none", X.dependent.lambda = FALSE, lambda.start = 100)), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], penalty = list(
-    homoscedastic = FALSE, X.dependent.lambda = FALSE,
-    lambda.start = NULL, c = 1.1, gamma = 0.1
-  )), "rlassoEffects")
-})
-
-test_that("rlassoEffect - Input check control", {
-  expect_is(rlassoEffect(X, y, d = X[, 1], control = list(numIter = 15, tol = 10^-4, threshold = 10^-3)), "rlassoEffects")
-  expect_is(rlassoEffect(X, y, d = X[, 1], control = list(numIter = 25)), "rlassoEffects")
-})
+patrick::with_parameters_test_that("Unit tests for rlassoAutoDML for ATE:",
+  .cases = test_cases, {
+    
+    set.seed(2)
+    if (!panel_data) {
+      data_gen <- DGP_contD(500, 10)
+      df <- data_gen$data
+      df$intercept <- rep(1, nrow(df))
+      Xnames <- colnames(df)[grep("V", colnames(df))]
+      backend_contD = DataAPDAutoDML(x = Xnames, d = "D", y = "y", data = df,
+                                     poly = poly_degree, interactions = interactions)
+    } else {
+      dgp_panel <- DGP_panel(1000, 10, 4)
+      DT_panel <- dgp_panel$data
+      Xnames = colnames(DT_panel)[grep("X", colnames(DT_panel))]
+      backend_contD <- DataAPDAutoDML(x = Xnames, d = "D", y = "y",
+                                            data = DT_panel,
+                                            poly_order = poly_degree,
+                                            interactions = interactions,
+                                            unit = "fixed",
+                                            time = "time")
+    }
+    
+    auto_apd = rlassoAutoDML(backend_contD, L = L,
+                             gamma_learner = gamma_learner,
+                             est_type = est_type,
+                             # weights = weights,
+                             debiased = debiased,
+                             D_LB = D_LB,
+                             D_add = D_add)
+    
+    
+    expect_is(backend_contD, "DataAPDAutoDML")
+    expect_is(auto_apd, "rlassoAutoDML")
+    expect_is(auto_apd$te, "numeric")
+  }
+)
